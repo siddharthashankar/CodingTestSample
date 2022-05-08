fe28a17d8f24983ca9b26cec300fc808ddba509a
SiddharthaShankar84 <shankar.siddhartha84@gmail.com>
2022-05-09 01:52:06 +0800

    Code Modified

--- app/src/main/java/com/sid/assignment/data/MovieApi.kt
+++ app/src/main/java/com/sid/assignment/data/MovieApi.kt
@@ -8,25 +8,25 @@ import retrofit2.http.Query
 interface MovieApi {
     @GET("movie/now_playing")
     fun getNowPlayingMovies(
-        @Query("api_key") apiKey: String = "c9c5ea53799624204822e99e30c87b54",
+        @Query("api_key") apiKey: String = "0e7274f05c36db12cbe71d9ab0393d47",
         @Query("page") page: Int
     ): Call<GetMoviesResponse>
 
     @GET("movie/popular")
     fun getPopularMovies(
-        @Query("api_key") apiKey: String = "c9c5ea53799624204822e99e30c87b54",
+        @Query("api_key") apiKey: String = "0e7274f05c36db12cbe71d9ab0393d47",
         @Query("page") page: Int
     ): Call<GetMoviesResponse>
 
     @GET("movie/top_rated")
     fun getTopRatedMovies(
-        @Query("api_key") apiKey: String = "c9c5ea53799624204822e99e30c87b54",
+        @Query("api_key") apiKey: String = "0e7274f05c36db12cbe71d9ab0393d47",
         @Query("page") page: Int
     ): Call<GetMoviesResponse>
 
     @GET("movie/upcoming")
     fun getUpcomingMovies(
-        @Query("api_key") apiKey: String = "c9c5ea53799624204822e99e30c87b54",
+        @Query("api_key") apiKey: String = "0e7274f05c36db12cbe71d9ab0393d47",
         @Query("page") page: Int
     ): Call<GetMoviesResponse>
 }
